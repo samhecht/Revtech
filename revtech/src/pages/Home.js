a95d68098c2fd298e47e6d2fc95aae27fe440899
Emily Zou <emily@d-172-25-230-219.dhcp.virginia.edu>
2019-06-03 10:36:16 -0400

    basic navbar

---
+++ revtech/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './../components/Navbar.js'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+function Home() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Navbar/>
+      <Box className={classes.root}>
+      <Typography variant="h1" component="h2" gutterBottom>
+        RevTek
+      </Typography>
+      </Box>
+    </div>
+  );
+
+}
+export default Home;
+
