3db0dbc2712e41a26166a9651a66424183283839
Emily Zou <emily@d-172-25-230-219.dhcp.virginia.edu>
2019-06-03 17:06:43 -0400

    logout

--- revtech/src/components/NavbarStudent.js
+++ revtech/src/components/NavbarStudent.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,10 +6,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom';
-import firebase from '../firebase/firebase'
-import { signOut } from "../firebase/firebase";
-
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -23,23 +20,8 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-// const handleSignOut = () => setUser(null);
-
-function Navbar(props){
+function NavbarStudent(){
     const classes = useStyles();
-    const [user, setUser] = useState(null); // sees if someone is logged in
-
-    function handleLogOut() {
-      setUser(null);
-      signOut().catch(e => console.error("Sign out error:", e));
-    }
-
-    function logoutButton() {
-      if (user == null) {
-        return <Button color="inherit" onClick={handleLogOut}><Link to="/">Sign Outtt</Link></Button>;
-      }
-    }
-
     return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -47,12 +29,11 @@ function Navbar(props){
           <Button color="inherit"><Link to="/">RevTek</Link></Button>
           <Button color="inherit"><Link to="/Companies">Companies</Link></Button>
           <Button color="inherit"><Link to="/Students">Students</Link></Button>
-          <Button color="inherit"><Link to="/SignIn">Login</Link></Button>
-          {user == null ? null : <Button color="inherit" onClick={handleLogOut}><Link to="/">Sign Out</Link></Button>}
+          <Button color="inherit"><Link to="/">Sign Out</Link></Button>
         </Toolbar>
       </AppBar>
     </div>
     );
 
 }
-export default Navbar;
+export default NavbarStudent;
