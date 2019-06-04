6fd98bdca8fd4d2867539d0f17ad3869fd9199b3
Maggie Bujor <mjb6ep@virginia.edu>
2019-06-04 14:31:28 -0400

    Created student profile

--- revtech/src/pages/Students.js
+++ revtech/src/pages/Students.js
@@ -13,6 +13,13 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import avatar from './avatar.png'
 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+
+import clsx from 'clsx';
+
 function Students(){
    
         return (
@@ -30,25 +37,33 @@ export default Students;
 
 
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
+
 
 function MediaCard() {
+    const useStyles = makeStyles({
+        card: {
+          maxWidth: 345,
+        },
+        media: {
+          height: 140,
+        },
+      });
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
 
   return (
     <Card className={classes.card}>
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          src={avatar}
-          title="Student Profile"
+        <img
+           className={classes.media}
+            src = {avatar}
+           //title="Student Profile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
@@ -66,7 +81,27 @@ function MediaCard() {
         <Button size="small" color="primary">
           Linkedin
         </Button>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="Show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+
+
       </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography list>Skills: </Typography>
+          Here are my skills
+     
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
