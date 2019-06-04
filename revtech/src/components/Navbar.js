08eb43ea48021712edff22b809a5d4b94d43a32a
Sammy Hecht <srh2kq@virginia.edu>
2019-06-04 13:47:37 -0400

    refactoring

--- revtech/src/components/Navbar.js
+++ revtech/src/components/Navbar.js
@@ -1,16 +1,11 @@
 import React, { useState }from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 import firebase from '../firebase/firebase'
 import { signOut } from "../firebase/firebase";
 import { Box } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
 
 
 
@@ -26,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-// const handleSignOut = () => setUser(null);
+
 
 function Navbar(props){
     const classes = useStyles();
@@ -57,48 +52,36 @@ function Navbar(props){
 
     return (
     <div className={classes.root}>
-      {/*}
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit"><Link to="/">RevTek</Link></Button>
-          <Button color="inherit"><Link to="/Companies">Companies</Link></Button>
-          <Button color="inherit"><Link to="/Students">Students</Link></Button>
-          {user !==null ? null : <Button color="inherit"><Link to="/SignIn">Login</Link></Button>}
-          {user == null ? null : <Button color="inherit" onClick={handleLogOut}><Link to="/">Logout</Link></Button>}
-        </Toolbar>
-      </AppBar>
-    */}
-
-    <Box
-      display='flex'
-      flex-direction='row'
-      justifyContent='space-between'
-      style={navBarStyle}
-    >
-      <Box
-        marginLeft="5%"
-      >
-        <Link style={linkStyle} to="/">
-          <Typography variant="h4" to="/" >
-            RevTek
-          </Typography>
-        </Link>
-      </Box>
-      
       <Box
         display='flex'
         flex-direction='row'
-        justifyContent='space-evenly'
-        color="inherit"
-        width="45%"
+        justifyContent='space-between'
+        style={navBarStyle}
       >
-        <Button color="inherit"><Link style={linkStyle} to="/Marketplace">Marketplace</Link></Button>
-        <Button color="inherit"><Link style={linkStyle} to="/Companies">Companies</Link></Button>
-        <Button color="inherit"><Link style={linkStyle} to="/Students">Students</Link></Button>
-        {user !==null ? null : <Button color="inherit"><Link style={linkStyle} to="/SignIn">Login</Link></Button>}
-        {user == null ? null : <Button color="inherit" onClick={handleLogOut}><Link style={linkStyle} to="/">Logout</Link></Button>}
+        <Box
+          marginLeft="5%"
+        >
+          <Link style={linkStyle} to="/">
+            <Typography variant="h4" to="/" >
+              RevTek
+            </Typography>
+          </Link>
+        </Box>
+        
+        <Box
+          display='flex'
+          flex-direction='row'
+          justifyContent='space-evenly'
+          color="inherit"
+          width="45%"
+        >
+          <Button color="inherit"><Link style={linkStyle} to="/Marketplace">Marketplace</Link></Button>
+          <Button color="inherit"><Link style={linkStyle} to="/Companies">Companies</Link></Button>
+          <Button color="inherit"><Link style={linkStyle} to="/Students">Students</Link></Button>
+          {user !==null ? null : <Button color="inherit"><Link style={linkStyle} to="/SignIn">Login</Link></Button>}
+          {user == null ? null : <Button color="inherit" onClick={handleLogOut}><Link style={linkStyle} to="/">Logout</Link></Button>}
+        </Box>
       </Box>
-    </Box>
     </div>
     );
 
