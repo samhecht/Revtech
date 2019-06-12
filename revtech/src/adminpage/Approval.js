8fa7fc6de4d6641993208cd3b2396bc427750d90
e-zou <emily.zou2@gmail.com>
2019-06-12 09:23:57 -0400

    new admin approval file

---
+++ revtech/src/adminpage/Approval.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Comment extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
