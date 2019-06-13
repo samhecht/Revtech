e9503989abd5eb608cda8ee14866203291ebdad4
maxrifkin <mfr3ds@virginia.edu>
2019-06-13 14:36:22 -0400

    profile pulls contracts from firebase

--- revtech/src/pages/ProfilePage.js
+++ revtech/src/pages/ProfilePage.js
@@ -13,75 +13,117 @@ import ProfilePosting from './../userpages/ProfilePosting.js'
 const text = "Immediately regret falling into bathtub woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now yet meow for stare out the window. Lick yarn hanging out of own butt. Stuff and things knock over christmas tree meow meow, i tell my human yet find empty spot in cupboard and sleep all day but i'm going to lap some water out of my master's cup meow";
 class ProfilePage extends React.Component {
   state = {
-   bio:"",
-   last:"",
-  
-   skills: ""
+    bio: "",
+    last: "",
+
+    skills: "",
+    commentIds: null, 
+    contractInfo:[{}]
 
   }
-  
-  componentDidMount(){
-    firebase.auth().onAuthStateChanged( user => {
-      if (user){
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
         var userId = firebase.auth().currentUser.uid;
-        const userRef = firebase.database().ref('students/'+userId);
-         console.log(userId)
-         userRef.on('value', (snapshot) => {
+        const commentsRef = firebase.database().ref('comments');
+        const userRef = firebase.database().ref('students/' + userId);
+        console.log(userId)
+        userRef.on('value', (snapshot) => {
           let data = snapshot.val();
-          
-             this.setState({
-              bio: data.bio,
-              last: data.last,
-              skills: data.skills,
-              userId: userId
-             })
-                  
+          this.setState({
+            bio: data.bio,
+            last: data.last,
+            skills: data.skills,
+            userId: userId
+          })
+        });
+        //fetch comments
+        commentsRef.on('value', (snapshot) => {
+          let tasks = snapshot.val();
+          let commentIds = [];
+          for (let task in tasks) {
+            if (tasks[task].userId == userId){
+                console.log(tasks[task].commentBody)
+                commentIds = commentIds.concat(tasks[task].parentContract)
+            }
+            
+          }
+
+          const contractsRef = firebase.database().ref('contracts');
+          contractsRef.on('value', (snapshot) => {
+            let contracts = snapshot.val();
+            let contractInfo = [];
+            var keys = Object.keys(contracts);
+           
+            for (let i = 0; i<keys.length;i++) {
+              
+             if(commentIds.includes(keys[i])){
+              
+                console.log(contracts[keys[i]].companyName)
+                let info = {
+                  company: contracts[keys[i]].companyName,
+                  description: contracts[keys[i]].description
+                }
+                contractInfo.push(info)
+             }
              
-          
-         
-          
-      });
-         
+            }
+            
+            console.log(contractInfo)
+            this.setState({contractInfo})
+          });
+        });
       }
     });
   }
 
+  mapContracts = () =>{
+    let contracts = this.state.contractInfo;
+    return contracts.map((item)=>{
+      return <ProfilePosting leftMargin={'0%'} width={'90%'} height={'200px'} arrow={false} company = {item.company} description = {item.description}/>
+    })
+
+  }
+
   render() {
     return (
-      <div className="appContainer " style = {{minHeight:1000 }}>
+      <div className="appContainer " style={{ minHeight: 1000 }}>
         <Navbar />
         <div className="profile1">
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start' }}>
             <img src="https://images.pexels.com/photos/259803/pexels-photo-259803.jpeg?cs=srgb&dl=adorable-animal-animal-photography-259803.jpg&fm=jpg" style={{ width: 300, height: 300, margin: 50 }} />
-            <div style={{ marginLeft: 50, marginBottom: 20, color: 'grey', fontSize:15 }}> Contact Info _________________________________</div>
+            <div style={{ marginLeft: 50, marginBottom: 20, color: 'grey', fontSize: 15 }}> Contact Info _________________________________</div>
             <div style={{ marginLeft: 50, fontSize: 20, display: 'flex', }}>
               LinkedIn
-              <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" style={{ width: 20, height: 20, marginLeft:5 }} />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" style={{ width: 20, height: 20, marginLeft: 5 }} />
             </div>
-            <a href="https://www.linkedin.com/in/cwransleriv/" style = {{marginLeft:50, marginTop:10}}>https://www.linkedin.com/in/cwransleriv/</a>
+            <a href="https://www.linkedin.com/in/cwransleriv/" style={{ marginLeft: 50, marginTop: 10 }}>https://www.linkedin.com/in/cwransleriv/</a>
             <div style={{ marginLeft: 50, marginTop: 30, fontSize: 20, display: 'flex', }}>
               GitHub
-              <img src="https://image.flaticon.com/icons/svg/25/25231.svg" style={{ width: 20, height: 20, marginLeft:5 }} />
+              <img src="https://image.flaticon.com/icons/svg/25/25231.svg" style={{ width: 20, height: 20, marginLeft: 5 }} />
             </div>
-            <a href="https://github.com/mfrifkin" style = {{marginLeft:50, marginTop:10}}>https://github.com/mfrifkin</a>
+            <a href="https://github.com/mfrifkin" style={{ marginLeft: 50, marginTop: 10 }}>https://github.com/mfrifkin</a>
           </div>
-          
+
           <div className="profile2" style={{ marginTop: 40, fontSize: 40, display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'start' }}>
             Kitty Cat Kevin
            <div style={{ fontSize: 10, display: 'flex', justifyContent: 'start' }}>HackCville Member</div>
-           <div style={{ fontSize: 30, display: 'flex', justifyContent: 'start', marginTop: 20}} >Bio</div>
-           {this.state.bio.length>0? <EditTextfield text = {this.state.bio} userId = {this.state.userId} />: <div></div>}
-           {/* <p style = {{ fontSize: 15, marginTop: 20, textAlign: 'left', maxWidth: 550 }}>{this.state.bio}</p> */}
-           <div className = "skillz" style={{ fontSize: 30, display: 'flex', justifyContent: 'start', flexDirection: 'row', height: '50px' }}> 
-           <div style = {{marginRight:'10px'}}>Skills: </div>
-    <div style = {{lineHeight: '16px'}}>{this.state.skills.length>0?<EditTextfieldSmall text = {this.state.skills} userId ={this.state.userId} />:<div></div>}</div>
+            <div style={{ fontSize: 30, display: 'flex', justifyContent: 'start', marginTop: 20 }} >Bio</div>
+            {this.state.bio.length > 0 ? <EditTextfield text={this.state.bio} userId={this.state.userId} /> : <div></div>}
+            
+            <div className="skillz" style={{ fontSize: 30, display: 'flex', justifyContent: 'start', flexDirection: 'row', height: '50px' }}>
+              <div style={{ marginRight: '10px' }}>Skills: </div>
+              <div style={{ lineHeight: '16px' }}>{this.state.skills.length > 0 ? <EditTextfieldSmall text={this.state.skills} userId={this.state.userId} /> : <div></div>}</div>
+
+            </div>
+            <div style={{ fontSize: 30, display: 'flex', justifyContent: 'start', marginTop: 20 }} >Active Contracts</div>
+
+            <div>{this.mapContracts()}</div>
+            
            
-           </div>
-           <div style={{ fontSize: 30, display: 'flex', justifyContent: 'start', marginTop: 20 }} >Active Contracts</div>
-           <ProfilePosting leftMargin = {'0%'} width = {'90%'} height = {'200px'} arrow={false}/>
-           {/* {this.state.bio.length>0? <EditTextfield text = {this.state.bio} userId = {this.state.userId} userKey = {this.state.userKey}/>: <div></div>} */}
           </div>
-         
+
         </div>
       </div>
 
